7033ccb3edd85fe1f9ba808b85390914d0d63556
MaximkaStar <max.martsinkus@mail.ru>
2019-01-03 18:28:53 +0600

    initial commit

---
+++ connectionApplicationForm/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace connectionApplicationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"DataSource=(local)\ANDRIANNA\SQLExpress;
+                                       Initial Catalog = databaseWPF;
+                                        Intergrated Security = True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Error." + ex.Message);
+            }
+        }
+    }
+}
